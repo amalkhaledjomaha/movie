a2fbd29a5f280512f30a93779d94b830e7d6e7a6
Rayan-alshtewi <rayanalshtewi52@gmail.com>
2026-08-28 18:12:06 +0200

    Initial project setup with splash screen

---
+++ lib/core/theme/app_string.dart
@@ -0,0 +1,33 @@
+class AppString {
+  static const String findYourNextFavoriteMovie="Find Your Next Favorite Movie Here";
+  static const String getAccessToHugeLibrary="Get access to a huge library of movies to suit all tastes. You will surely like it.";
+
+  static const String discoverMovies="Discover Movies";
+  static const String discoverMoviesDescription="Explore a vast collection of movies in all qualities and genres."
+      " Find your next favorite film with ease.";
+
+  static const String exploreAllGenres="Explore All Genres";
+  static const String exploreAllGenresDescription="Discover movies from every genre, in all available qualities."
+      " Find something new and exciting to watch every day.";
+
+
+  static const String createWatchlists="Create Watchlists";
+  static const String createWatchlistsDescription="Save movies to your watchlist to keep track of what you want to watch next. "
+      "Enjoy films in various qualities and genres.";
+
+  static const String rateReviewAndLearn="Rate, Review, and Learn";
+  static const String rateReviewAndLearnDescription="Share your thoughts on the movies you've watched."
+      " Dive deep into film details and help others discover great movies with your reviews..";
+
+  static const String startWatchingNow="Start Watching Now";
+  static const String startWatchingNowDescription="Share your thoughts on the movies you've watched."
+      " Dive deep into film details and help others discover great movies with your reviews..";
+
+  static const String next="Next";
+  static const String back="Back";
+  static const String finish="Finish";
+  static const String exploreNow="Explore Now";
+  static const String email="Email";
+  static const String verifyEmail="Verify Email";
+  static const String forgetPassword="Forget Password";
+}
