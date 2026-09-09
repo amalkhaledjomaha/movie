91c7887ea2f369ba5aa1681bfb3d42d6ce9d0e2d
aishagrada <aishagrada24@gmail.com>
2026-09-09 12:47:15 +0200

    Language changed

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,289 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get findYourNextFavoriteMovie => 'Find Your Next\nFavorite Movie Here';
+
+  @override
+  String get getAccessToHugeLibrary => 'Get access to a huge library of movies to suit all tastes. You will surely like it.';
+
+  @override
+  String get discoverMovies => 'Discover Movies';
+
+  @override
+  String get discoverMoviesDescription => 'Explore a vast collection of movies in all qualities and genres. Find your next favorite film with ease.';
+
+  @override
+  String get exploreAllGenres => 'Explore All Genres';
+
+  @override
+  String get exploreAllGenresDescription => 'Discover movies from every genre, in all available qualities. Find something new and exciting to watch every day.';
+
+  @override
+  String get createWatchlists => 'Create Watchlists';
+
+  @override
+  String get createWatchlistsDescription => 'Save movies to your watchlist to keep track of what you want to watch next. Enjoy films in various qualities and genres.';
+
+  @override
+  String get rateReviewAndLearn => 'Rate, Review, and Learn';
+
+  @override
+  String get rateReviewAndLearnDescription => 'Share your thoughts on the movies you\'ve watched. Dive deep into film details and help others discover great movies through your reviews.';
+
+  @override
+  String get startWatchingNow => 'Start Watching Now';
+
+  @override
+  String get startWatchingNowDescription => 'Share your thoughts on the movies you\'ve watched. Dive deep into film details and help others discover great movies through your reviews.';
+
+  @override
+  String get next => 'Next';
+
+  @override
+  String get back => 'Back';
+
+  @override
+  String get finish => 'Finish';
+
+  @override
+  String get exploreNow => 'Explore Now';
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get verifyEmail => 'Verify Email';
+
+  @override
+  String get forgetPassword => 'Forget Password';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get forgetenpaword => 'Forget Password ?';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get donthaveaccount => 'Don’t Have Account ?';
+
+  @override
+  String get createone => 'Create One';
+
+  @override
+  String get or => 'OR';
+
+  @override
+  String get loginwithgoogle => 'Login With Google';
+
+  @override
+  String get register => 'Register';
+
+  @override
+  String get name => 'Name';
+
+  @override
+  String get confirmpassword => 'Confirm Password';
+
+  @override
+  String get phonenumber => 'Phone Number';
+
+  @override
+  String get createaccount => 'Create Account';
+
+  @override
+  String get alreadyhaveaccount => 'Already Have Account ?';
+
+  @override
+  String get pickavatar => 'Pick Avatar';
+
+  @override
+  String get johnsafwat => 'John Safwat';
+
+  @override
+  String get resetpassword => 'Reset Password';
+
+  @override
+  String get deleteaccount => 'Delete Account';
+
+  @override
+  String get updatedata => 'Update Data';
+
+  @override
+  String get nameisrequired => 'Name is required';
+
+  @override
+  String get namemustbeatleast3characters => 'Name must be at least 3 characters';
+
+  @override
+  String get confirmpasswordisrequired => 'Please confirm your password';
+
+  @override
+  String get passwordsdonotmatch => 'Passwords do not match';
+
+  @override
+  String get phoneisrequired => 'Phone is required';
+
+  @override
+  String get phonethan10 => 'Phone must be at least 10 characters';
+
+  @override
+  String get invalidphone => 'Enter a valid phone number';
+
+  @override
+  String get accountupdated => 'Account updated successfully';
+
+  @override
+  String get updateerror => 'Failed to update account';
+
+  @override
+  String get deleteaccounttitle => 'Delete Account';
+
+  @override
+  String get deleteaccountmessage => 'Are you sure you want to delete your account?';
+
+  @override
+  String get cancel => 'Cancel';
+
+  @override
+  String get delete => 'Delete';
+
+  @override
+  String get loginagaintodelete => 'Please login again to delete your account';
+
+  @override
+  String get deleteerror => 'Failed to delete account';
+
+  @override
+  String get emailisrequired => 'Email is required';
+
+  @override
+  String get enteravalidemail => 'Enter a valid email';
+
+  @override
+  String get passwordisrequired => 'Password is required';
+
+  @override
+  String get passwordmustbeatleast6characters => 'Password must be at least 6 characters';
+
+  @override
+  String get invalid_credential => 'invalid-credential';
+
+  @override
+  String get emailorpasswordisincorrect => 'Email or password is incorrect';
+
+  @override
+  String get user_not_found => 'user-not-found';
+
+  @override
+  String get noaccountfoundwiththisemail => 'No account found with this email';
+
+  @override
+  String get wrong_password => 'wrong-password';
+
+  @override
+  String get incorrectPassword => 'Incorrect password';
+
+  @override
+  String get invalid_email => 'invalid-email';
+
+  @override
+  String get pleaseenteravalidemail => 'Please enter a valid email';
+
+  @override
+  String get too_many_requests => 'too-many-requests';
+
+  @override
+  String get too_many_attempts => 'Too many attempts. Please try again later.';
+
+  @override
+  String get something_went_wrong => 'Something went wrong. Please try again.';
+
+  @override
+  String get login_title => 'Login Failed';
+
+  @override
+  String get ok => 'OK';
+
+  @override
+  String get title_success => 'Success';
+
+  @override
+  String get desc_Success => 'Password reset link has been sent to your email.';
+
+  @override
+  String get title_error => 'Reset Password Failed';
+
+  @override
+  String get accountcreatedsuccessfully => 'Account created successfully';
+
+  @override
+  String get home => 'Home';
+
+  @override
+  String get search => 'Search';
+
+  @override
+  String get browse => 'Browse';
+
+  @override
+  String get profile => 'Profile';
+
+  @override
+  String get somethingwentwrong => 'Something went wrong';
+
+  @override
+  String get weakpassword => 'The password is too weak';
+
+  @override
+  String get emailalreadyinuse => 'This email is already registered';
+
+  @override
+  String get invalidemail => 'The email address is not valid';
+
+  @override
+  String get operationnotallowed => 'Email/Password authentication is not enabled';
+
+  @override
+  String get networkrequestfailed => 'Please check your internet connection';
+
+  @override
+  String get watchnow => 'Watch Now';
+
+  @override
+  String get action => 'Action';
+
+  @override
+  String get seemore => 'See More →';
+
+  @override
+  String get availablenow => 'Available Now';
+
+  @override
+  String get romance => 'Romance';
+
+  @override
+  String get comedy => 'Comedy';
+
+  @override
+  String get horror => 'Horror';
+
+  @override
+  String get thriller => 'Thriller';
+
+  @override
+  String get sciFi => 'Sci-Fi';
+
+  @override
+  String get movieapp => 'Movie App';
+}
